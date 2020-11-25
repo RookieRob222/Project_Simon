896bb67511c89b45076f4a885fb901828dce027e
sbarrett707 <sbarrett707@gmail.com>
2020-11-25 18:39:22 -0500

    Added Mongoose Connection

---
+++ models/dbmodel.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var Schema =mongoose.Schema;
+var User = require('./userModel');
+
+
+function createUser(username,password){
+import.login.js(username);
+import.login.js(password);
+userSchema.insert(username) as new userSchema.username;
+userSchema.insert(password) as new userSchema.password;
+}
+function getUser(username){
+    use simonDB.User;
+    User.find(username.current);
+ 
+}
+
+function getAllUsers(){
+    use simonDB.User;
+    User.find(username).pretty;
+}
+
+function setScore(score,highscore){
+    use simonDB.User;
+    User.update($set : highscore=score);
+}
+
+function getScore(){
+    use simonDB.User;
+    User.find(curentuser:(highscore));
+}
+
+function getAllScores(){
+    use simonDB.User;
+    User.find(username);
+}
